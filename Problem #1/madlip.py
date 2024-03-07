7e0d2bc753fcf5f9307921fd0c54558f0bb0ae2d
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-03-07 12:31:48 -0600

    problem 2

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -1,8 +1,6 @@
 def food_input():
-    user_input = input()
-    tokens = user_input.split()
-    #type you while  loop here
-
+    mad_dictionary = {}
+    
     
 
 if __name__ == "__main__":
--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,5 +1,9 @@
 def check_palindrome(user_input):
- #type your code here
+ inv_input = user_input[::-1]
+ if inv_input == user_input:
+    print("Palindrome: "+user_input)
+ else:
+    print("Not a palindrome: "+user_input)
 if __name__ == "__main__":
     user_input = input()
     check_palindrome(user_input)
