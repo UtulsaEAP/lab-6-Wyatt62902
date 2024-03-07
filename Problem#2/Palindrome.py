4a4c814bc40caad6315214db6acd58a1b1a0b64d
Wyatt62902 <156848807+Wyatt62902@users.noreply.github.com>
2024-03-07 17:40:37 -0600

    problem 2

--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,9 +1,13 @@
+'''
+Name: Wyatt Fulton
+Lab Time: 2:00 PM
+'''
 def check_palindrome(user_input):
  inv_input = user_input[::-1]
  if inv_input == user_input:
-    print("Palindrome: "+user_input)
+    print("palindrome: "+user_input)
  else:
-    print("Not a palindrome: "+user_input)
+    print("not a palindrome: "+user_input)
 if __name__ == "__main__":
     user_input = input()
     check_palindrome(user_input)
